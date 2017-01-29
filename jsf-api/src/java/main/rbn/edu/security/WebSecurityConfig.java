e073ae88d78c1772923b7a94a7e8532efdb1abc5
Rafael Nanes <rafaelbraga_22@hotmail.com>
2017-01-29 19:48:33 -0300

    permitindo qq role entrar no index

--- jsf-api/src/java/main/rbn/edu/security/WebSecurityConfig.java
+++ jsf-api/src/java/main/rbn/edu/security/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/app/user/**").access("hasRole('ROLE_ADMIN')")//
 		.antMatchers("/app/product/**").access("hasRole('ROLE_ADMIN')")//
 		.antMatchers("/app/customer/**").access("hasAnyRole('ROLE_CUSTOMER')")//
-		.antMatchers("/index.xhtml").access("hasAnyRole('ROLE_VISITOR')")//
+		.antMatchers("/index.xhtml").access("hasAnyRole('ROLE_VISITOR', 'ROLE_CUSTOMER', 'ROLE_ADMIN')")//
 		.and().exceptionHandling().accessDeniedPage("/403.xhtml")
 
 		.and().formLogin().loginPage("/login.xhtml").loginProcessingUrl("/appLogin")
